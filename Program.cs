2325e02d81147b4b276774c9bdee89206365464a
Vyacheslav <vyacheslav.popov.qa@gmail.com>
2025-11-02 23:12:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using csharp_money_homework;
+
+Money money = new Money();
+
+Console.WriteLine("Введите первое число");
+(bool isFirstAmountCorrect, int firstNum) = money.ValidateAmount(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+(bool isSecondAmountCorrect, int secondNum) = money.ValidateAmount(Console.ReadLine());
+
+money.SetAmount(firstNum, secondNum);
+
+Console.WriteLine($"{money.MainAmount},{money.SecondAmount}");
